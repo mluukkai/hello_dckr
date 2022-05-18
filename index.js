7fcdd2efd7a52cf4c8ac653d2cf8893ec6822755
Matti Luukkainen <mluukkai@iki.fi>
2022-05-18 23:02:58 +0300

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  res.send("<h1>Hello Wurld!</h1>");
 });
 
 app.get("/ping", (req, res) => {
